4e71901cd2b9b2828fb2d250acbd88c53fab83c3
Shukrah <yusufshukurah59@gmail.com>
2022-03-10 08:16:53 -0800

    Displaying

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*This is a multi line comment that pronts out a string using puts and returns 0 as an output
+*/
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return 0;
+}
