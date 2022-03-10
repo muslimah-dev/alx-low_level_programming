7d17ebc6098879573f67e17bc542aa67e382d8e9
Shukrah <yusufshukurah59@gmail.com>
2022-03-10 09:37:19 -0800

    Display

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-#​include​ ​<​stdio.h​> 
-  
- ​/*​
- ​ * the main part
- ​ * 
- ​ * this is a multiline comment that prints a string and returns 0 
- ​ * 
- ​ * it turns to be successful when it returns 0
- ​ ​*/ 
- ​int​ ​main​(​void​) 
- {
- ​        ​puts​(​"​\"​Programming is like building a multilingual puzzle​"​); 
- ​        ​return​ (​0​); 
- ​}
+#include <stdio.h>
+/*
+ * the main part
+ *
+ *Description : prints a string
+ *
+ *Returns success!
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle"
+);
+	return(0);
+}
