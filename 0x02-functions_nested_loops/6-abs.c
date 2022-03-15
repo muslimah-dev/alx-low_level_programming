e8931d063e4b7e4ff8bd828247e5d297be6bc1a6
Shukrah <yusufshukurah59@gmail.com>
2022-03-15 12:10:58 -0700

    absolutes

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the avsolute value of an integer
+ * @n: variable used.
+ * Return: absolute value if int.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,5 @@ void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
 void print_alphabet_x10(void);
