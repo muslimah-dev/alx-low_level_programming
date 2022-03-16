8210e5f20cbfa72d757bf2269dd87db836b13fc3
Shukrah <yusufshukurah59@gmail.com>
2022-03-16 08:07:34 -0700

    lala

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints fibonacci numbers less than 50
+ * Return: 0, success.
+ */
+int main(void)
+{
+	long int i, j, k, next;
+
+	j = 1;
+	k = 2;
+
+	for (i = 0; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		} else
+		{
+			printf("%ld\n", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	return (0);
+}
