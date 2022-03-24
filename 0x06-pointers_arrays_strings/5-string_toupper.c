90ff7c1baebc8132ab1550a48e3128b4015b35f7
Shukrah <yusufshukurah59@gmail.com>
2022-03-24 00:17:38 -0700

    lala

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,16 +6,17 @@
  * @s: variable used.
  * Return: 0
  */
-char *string_toupper(char *s[])
+char *string_toupper(char *s)
 {
 	int c = 0;
 
-	while (*s[c] != '\0')
+	while (s[c] != '\0')
 	{
-		if (*s[c] >= 'a' && *s[c] <= 'z')
+		if (s[c] >= 'a' && s[c] <= 'z')
 		{
-			*s[c] = *s[c] - 32;
+			s[c] = s[c] - 32;
 		}
 		c++;
 	}
+	return (s);
 }
