a3d7db0a78d23db1f5677aef3080a8032e9c6559
Yusuf Oluwadamilola <yoluwadamilola79@gmail.com>
2022-04-22 11:24:33 +0000

    lala

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Write a function that print
+ * all the elements if lists_t list
+ * @h: the node.
+ * Return: number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		} else
+		{
+			printf("[%u]%s",h->len, h->str);
+			i++;
+		}
+	}
+	return (i);
+}
