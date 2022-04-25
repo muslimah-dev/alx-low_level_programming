7d32162e012265e3f688d4f06bf16d7a7b02da9f
Yusuf Oluwadamilola <yoluwadamilola79@gmail.com>
2022-04-25 07:54:23 +0000

    lala

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ /**
+ * first - write a function that prints
+ * a string before main function is execute
+ * Description - it uses the printf function
+ * d
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
