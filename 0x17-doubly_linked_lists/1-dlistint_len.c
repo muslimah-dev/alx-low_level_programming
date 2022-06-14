bdd2e19ea6946c368ab44b0aac7c969b8ca1a0d1
Oluwadamilola <suncodesy@gmail.com>
2022-06-14 19:25:26 +0000

    create 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,6 +3,7 @@
 /**
  * add_dnodeint - adds a new node at the beginning
  * of a dlistint_t list
+ *
  * @head: head of the list
  * @n: value of the element
  * Return: the address of the new element
@@ -13,14 +14,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *h;
 
 	current = malloc(sizeof(dlistint_t));
-	if (!current)
+	if (current == NULL)
 		return (NULL);
 
 	current->n = n;
 	current->prev = NULL;
 	h = *head;
 
-	if (h)
+	if (h != NULL)
 	{
 		while (h->prev != NULL)
 			h = h->prev;
@@ -28,7 +29,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	current->next = h;
 
-	if (h)
+	if (h != NULL)
 		h->prev = current;
 
 	*head = current;
