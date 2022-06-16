c840ef52e9fcb1a835073e19cfd0c95e7e8d0dec
Oluwadamilola <suncodesy@gmail.com>
2022-06-16 13:02:46 +0000

    create 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
- /**
- * first - write a function that prints
- * a string before main function is execute
- * Description - it uses the printf function
- * d
+/**
+ * bmain - function executed before main
+ * Return: no return.
  */
-void first(void)
+
+void __attribute__ ((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
